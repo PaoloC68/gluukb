733ff1e9b92c62318ca1c4f7eca4bd75792924e5
PaoloC68 <paolo@calvitecnologie.it>
2013-04-25 16:21:57 -0300

    better default routes

--- gluukb/urls.py
+++ gluukb/urls.py
@@ -8,8 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^$', knowledge_index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^knowledge/', include('knowledge.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^', include('knowledge.urls')),
 )
