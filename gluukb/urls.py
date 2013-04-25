d1630596d525ade0dd1379e44f558a9fc3a76202
PaoloC68 <paolo@calvitecnologie.it>
2013-04-25 16:08:11 -0300

    added default route

--- gluukb/urls.py
+++ gluukb/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from knowledge.views import knowledge_index
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,6 +8,7 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
+    url(r'^$', knowledge_index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^knowledge/', include('knowledge.urls')),
     url(r'^grappelli/', include('grappelli.urls')),
