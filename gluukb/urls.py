29114aad6e4d0a9b57a071fe9e7a978122745e95
PaoloC68 <paolo@calvitecnologie.it>
2013-05-19 23:22:09 -0300

    new layout

--- gluukb/urls.py
+++ gluukb/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from knowledge.views import knowledge_index
+from sugarlink.views import logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,4 +13,6 @@ urlpatterns = patterns(
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^', include('knowledge.urls')),
     url(r'', include('social_auth.urls')),
+    url(r'^logout/$', logout , name='logout'),
+    #url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
 )
--- sugarlink/views.py
+++ sugarlink/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.contrib import auth
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('knowledge_index'))
